64774db05d95fdc7d92ee0de1308e945105f4cd2
Sumitkalamkar <167060805+Sumitkalamkar@users.noreply.github.com>
2026-03-03 22:16:04 +0530

    Implement findKthBit function in Solution class

---
+++ Find Kth Bit in Nth Binary String.py
@@ -0,0 +1,24 @@
+class Solution:
+    def findKthBit(self, n: int, k: int) -> str:
+        
+        # Base case
+        if n == 1:
+            return "0"
+        
+        length = (1 << n) - 1      # 2^n - 1
+        mid = (length // 2) + 1    # middle position
+        
+        # middle bit
+        if k == mid:
+            return "1"
+        
+        # left half
+        if k < mid:
+            return self.findKthBit(n-1, k)
+        
+        # right half
+        mirror = length - k + 1
+        bit = self.findKthBit(n-1, mirror)
+        
+        # invert bit
+        return "1" if bit == "0" else "0"
